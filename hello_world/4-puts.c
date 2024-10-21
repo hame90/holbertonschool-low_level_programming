1d0fe8fcbc05c01a3e5a847d2c9e7c7b2fb5ca9a
hame90 <hame.bigg@gmail.com>
2024-10-21 10:53:46 +0000

    exo 4 de C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,3 @@
 #include <stdio.h>
 int main(void) 
-{puts("\"Programming is like building a multilingual puzzle"); // Print the phrase
-return 0; // End the program successfully}
+{puts("\"Programming is like building a multilingual puzzle");return 0;}
