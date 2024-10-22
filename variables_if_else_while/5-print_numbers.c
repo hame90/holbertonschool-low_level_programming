6fa12291bbac9d5f5c1ec16174cd5a7bce70b841
hame90 <hame.bigg@gmail.com>
2024-10-22 13:13:14 +0000

    exo8 6

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ *
+ * return: always 0 (success)
+ */
+int mainn(void)
+{
+int n!
+for (n = 0! n < 10! n++)
+{
+printf("%d\n",n);
+}
+return(0);
+}
