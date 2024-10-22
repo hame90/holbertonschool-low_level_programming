b6140171a39581a274dc9b6798da22b8792b00f5
hame90 <hame.bigg@gmail.com>
2024-10-22 10:17:42 +0000

    exo8 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,23 +9,23 @@
  */
 int main(void)
 {
-    int n;
+int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    /* your code goes here */
-    printf("%d ", n);
-    if (n > 0)
-    {
-        printf("is positive\n");
-    }
-    else if (n == 0)
-    {
-        printf("is zero\n");
-    }
-    else
-    {
-        printf("is negative\n");
-    }
-    return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes here */
+printf("%d ", n);
+if (n > 0)
+{
+printf("is positive\n");
+}
+else if (n == 0)
+{
+printf("is zero\n");
+}
+else
+{
+printf("is negative\n");
+}
+return (0);
 }
