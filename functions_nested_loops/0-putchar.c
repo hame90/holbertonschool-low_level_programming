cff22e6f47962ef9623d4b9bb593e12ee1710c7d
hame90 <hame.bigg@gmail.com>
2024-10-24 09:05:53 +0000

    exo 9 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -7,7 +7,13 @@
  */
 int main(void)
 {
-printf("_putchar\n");
+char *str = "_putchar";
+while (*str)
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+
 return (0);
 }
-
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,6 @@
-je suis la 
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+#endif /* MAIN_H */ 
