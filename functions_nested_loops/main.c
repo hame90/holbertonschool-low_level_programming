0ab4fb52c3924eff9d0424c9c4e685d5ccbc63be
hame90 <hame.bigg@gmail.com>
2024-10-28 16:30:49 +0100

    hjgjdfdf

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -3,17 +3,17 @@
 
 int main(void)
 {
-char c = 'a';
-if (_islower(c))
-printf("%c is lowercase\n", c);
-else
-printf("%c is not lowercase\n", c);
+	char c = 'a';
+	if (_islower(c))
+		printf("%c is lowercase\n", c);
+	else
+		printf("%c is not lowercase\n", c);
 
-c = 'A';
-if (_islower(c))
-printf("%c is lowercase\n", c);
-else
-printf("%c is not lowercase\n", c);
+	c = 'A';
+	if (_islower(c))
+		printf("%c is lowercase\n", c);
+	else
+		printf("%c is not lowercase\n", c);
 
-return (0);
+	return (0);
 }
