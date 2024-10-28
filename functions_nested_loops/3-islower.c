b63fb89a282752ed27ec05b4a1035a1418093f3b
hame90 <hame.bigg@gmail.com>
2024-10-28 14:41:20 +0100

    exoo

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,16 +2,16 @@
 #include "main.h"
 
 /**
-*_islower - checks for lowercase character
-*@c: the character to check
-* 
-*Return: 1 if c is lowercase, 0 otherwise
-*/
+ *_islower - checks for lowercase character
+ *@c: the character to check
+ * 
+ *Return: 1 if c is lowercase, 0 otherwise
+ */
 int _islower(int c)
 {
-if (c >= 'a' && c <= 'z')
-{
-return (1);
-}
-return (0);
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAiN_H
-
+int _putchar(char c);
+int main(void);
 void print_alphabet_x10(void);
 void print_alphabet(void);
 int _islower(int c);
