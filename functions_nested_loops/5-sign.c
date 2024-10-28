bff139b4879f24fe7a7080b53ce0e03e2ec425bb
hame90 <hame.bigg@gmail.com>
2024-10-28 15:26:07 +0100

    exoo

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *  * print_sign - sign of a number.
- *   * @n: check the sign of a number
- *    * Return: 1 if n is greater than 0, 0 if 0 and -1 if less than 0.
- *     */
+ * print_sign - sign of a number.
+ * @n: check the sign of a number
+ * Return: 1 if n is greater than 0, 0 if 0 and -1 if less than 0.
+ */
+
 int print_sign(int n)
 {
 	if (n > 0)
