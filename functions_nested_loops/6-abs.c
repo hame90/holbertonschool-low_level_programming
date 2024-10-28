d0f1dffe0d97c91e09d802fec6437bf63466bc34
hame90 <hame.bigg@gmail.com>
2024-10-28 15:30:42 +0100

    exoo

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+include <stdio.h>
+#include "main.h"
+/**
+ * _abs - calcule la valeur absolue d'un entier.
+ * @c: l'entier dont on veut la valeur absolue.
+ * Return: la valeur absolue de n.
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		return (-c);
+	}
+	else
+	{
+		return (c);
+	}
+}
