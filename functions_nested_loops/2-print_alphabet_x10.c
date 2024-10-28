6523afb0ec0b8a1fccae704dcdca6dcb7931a893
hame90 <hame.bigg@gmail.com>
2024-10-28 09:50:33 +0100

    exo3 3

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,20 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_alphabet_x10 - prints the alphabet in lowercase 10 times, followed by a new line
+ * print_alphabet_x10 - prints the alphabet 10 time in lowercase
  */
 void print_alphabet_x10(void)
 {
-char ch[] = "abcdefghijklmnopqrstuvwxyz";
-int i, j;
+int i,
+char letter;
 
-for (j = 0; j < 10; j++)
+for (i = 0; i < 10; i++)
 {
-for (i = 0; i < 26; i++)
+letter = 'a';
+while (letter <= 'z')
 {
-_putchar(ch[i]);
+_putchar(letter);
+letter++;
 }
 _putchar('\n');
 }
