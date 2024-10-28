f983833c20f553b2202dc3d24870fe4dd479e20b
hame90 <hame.bigg@gmail.com>
2024-10-28 15:11:12 +0100

    esxxoo

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic characters
+ * @c: checks characters
+ * return: 1 if c is a letter,lowcase or uppercase else 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
+	{
+		return (1);
+	}
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,5 +5,5 @@ int main(void);
 void print_alphabet_x10(void);
 void print_alphabet(void);
 int _islower(int c);
-
+int _isalpha(int c);
 #endif /* MAIN.H */
