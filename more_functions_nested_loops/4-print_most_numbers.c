c8a3e546bb3185ed7a203192b281d0b594800a8b
hame90 <hame.bigg@gmail.com>
2024-10-28 17:36:43 +0100

    hjjhydf

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - printf the number.
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int b;
+
+	for (b = '0'; b <= '9'; b++)
+	{
+		if (b == '2'|| b == '4')
+		{ 
+			continue;
+		}
+		_putchar(b);
+	}
+	_putchar('\n');
+}
