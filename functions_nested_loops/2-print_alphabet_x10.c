c2008cd3e6ca054e83e97c17aab9e99070bb719f
hame90 <hame.bigg@gmail.com>
2024-10-28 10:52:37 +0100

    exo3 3

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_alphabet_x10 - prints the alphabet 10 time in lowercase
  */
 void print_alphabet_x10(void)
 {
-int i,
-char letter;
+char i;
+int y;
 
-for (i = 0; i < 10; i++)
+for (y ='0'; y <= '9'; y++)
 {
-letter = 'a';
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
+   for (i='a'; i <='z'; i++)
+   {
+	   _putchar(i);
 }
 _putchar('\n');
 }
