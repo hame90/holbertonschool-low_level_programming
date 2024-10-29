d909f3beae7e9b44733f4a8e3ed40d1eb9a490ab
hame90 <hame.bigg@gmail.com>
2024-10-29 12:31:43 +0100

    hgdf

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/** 
+ * print_line - that draws a straight line in the terminal.
+ * @n: draws a straight 
+ * return: \n if n is 0 or less.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+   	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
