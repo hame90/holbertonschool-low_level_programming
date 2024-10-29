ade7ab84c464c9773d16ca349232d28d828ff9c9
hame90 <hame.bigg@gmail.com>
2024-10-29 12:36:45 +0100

    update

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "main.h"
-/** 
+/**
  * print_line - that draws a straight line in the terminal.
- * @n: draws a straight 
+ * @n: draws a straight
  * return: \n if n is 0 or less.
  */
 void print_line(int n)
@@ -11,12 +11,13 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-   	for (i = 0; i < n; i++)
-	{
-		_putchar('_');
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
 	}
 	_putchar('\n');
-	}
+
 	else
 	{
 		_putchar('\n');
