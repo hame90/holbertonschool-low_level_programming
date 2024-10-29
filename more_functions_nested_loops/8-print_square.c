e0d40ee3658b35bef0e96f54cbc328a5fdfbb902
hame90 <hame.bigg@gmail.com>
2024-10-29 16:26:31 +0100

    hghdf

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,26 +8,21 @@
  */
 
 void print_square(int size)
-{
-	int i, j;
-
-	if (size < 0)
+{ 
+	int i, x;
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (x = 0; x < size; x++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}       	
+	else
 	{
 		_putchar('\n');
 	}
-
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			_putchar('#');
-		}
-		{
-		else
-		}
-		_putchar('\n');
-	}
-
 }
-
-
