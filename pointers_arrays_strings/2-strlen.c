e9569992e0d8f6a576c2375b3597c8614d6258e2
hame90 <hame.bigg@gmail.com>
2024-10-31 12:46:55 +0100

    hdhdhjhhj

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strlen - returns the length of a string
+* @s: the string to measure
+* Return: the length of the string
+*/
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+		length++;
+
+	return length;
+}
+
